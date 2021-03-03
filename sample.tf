e871589405c7e41d0e63e1f1c2434d84e870b952
Pavan Kumar <pavan_2004it@icloud.com>
2021-03-03 09:47:00 +0530

    Added Sg

--- sample.tf
+++ sample.tf
@@ -26,9 +26,31 @@ resource "aws_instance" "test" {
   associate_public_ip_address = true
   instance_type = "t2.medium"
   key_name = "tf-test"
+  vpc_security_group_ids = [ "aws_security_group.ansi.id" ]
   tags = {
       Environment = "dev"
       Name = "test"
+  }
+}
+
+resource "aws_security_group" "ansi" {
+  name = "ansi"
+  description = "Sg for ansible"
+  vpc_id = "vpc-08040ada704ce9276"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
